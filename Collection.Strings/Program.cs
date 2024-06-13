63be75cd29aa02472ed8ff16e3a6d5b46b992c89
James Luncan <jluncan@tql.com>
2024-06-13 16:33:15 -0400

    Add project files.

---
+++ Collection.Strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+
+namespace Collection.Strings {
+    internal class Program {
+        static void Main(string[] args) {
+
+            /*
+
+
+            List<string> strings = new List<string>();
+
+            strings.Add("Pink");
+            strings.Add("Red");
+            strings.Add("Magenta");
+            strings.Add("Burgundy");
+            strings.Add("Black");
+
+            strings.AddRange(new string[] { "Green", "Yellow" });
+
+            Console.Write("Enter your color here");
+            var color = Console.ReadLine();
+            if (strings.Contains(color)) {
+                Console.WriteLine("color already exists!");
+            } else {
+                strings.Add(color);
+                Console.WriteLine("Color added");
+            }
+                }
+                }
+         */
+
+
+          
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(1, "one");
+            dictionary.Add(22, "Twenty-two");
+            dictionary.Add(14, "Fourteen");
+
+            Console.WriteLine(dictionary[22]); 
+
+        }
+    }
+}
